f4298673ac9f758c404aebd5fbd37d1168921b8b
gian <gianfranco.jsx@gmail.com>
2023-05-11 12:39:04 -0300

    fix(Login) localStorage in server

--- src/shared/hooks/useLogin.tsx
+++ src/shared/hooks/useLogin.tsx
@@ -1,41 +1,60 @@
 import { useEffect, useState } from "react";
 
+const isServer = typeof window === "undefined";
+
+const manageLocalStorage = {
+    get: {
+        token: !isServer ? localStorage.getItem("token") : null,
+        user: !isServer ? localStorage.getItem("user") : null
+    },
+    set: {
+        token: (token: string) => !isServer ? localStorage.setItem("token", token) : null,
+        user: (user: string) => !isServer ? localStorage.setItem("user", user) : null
+    },
+    remove: {
+        token: () => !isServer ? localStorage.removeItem("token") : null,
+        user: () => !isServer ? localStorage.removeItem("user") : null
+    }
+};
+
+
+
 const useLogin = () => {
     const [login, setLogin] = useState<boolean>(false);
     const [token, setToken] = useState<string | null>(null);
     const [user, setUser] = useState<string | null>(null);
 
     useEffect(() => {
-        const token = localStorage.getItem("token");
+        const token = manageLocalStorage.get.token;
         if (token) {
             setLogin(true);
         }
     }, []);
 
     useEffect(() => {
-        const previousToken = localStorage.getItem("token");
+        const previousToken = manageLocalStorage.get.token;
         if (token && token !== previousToken) {
-            localStorage.setItem("token", token);
+           manageLocalStorage.set.token(token);
         }
     }, [token]);
 
     useEffect(() => {
         const previousUser = localStorage.getItem("user");
         if (user && user !== previousUser) {
-            localStorage.setItem("user", user);
+            manageLocalStorage.set.user(user);
         }
     }, [user]);
 
     const logout = () => {
-        localStorage.removeItem("token");
-        localStorage.removeItem("user");
+        manageLocalStorage.remove.token();
+        manageLocalStorage.remove.user();
         setLogin(false);
     };
 
     return {
         login,
-        user: user || localStorage.getItem("user"),
-        token: token || localStorage.getItem("token"),
+        user: user || manageLocalStorage.get.user,
+        token: token || manageLocalStorage.get.token,
         setToken,
         setUser,
         logout
